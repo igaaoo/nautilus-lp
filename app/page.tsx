c48462b15e53f5cf4b3d1810616cf57856597ae2
igaaoo <igorneves0404@gmail.com>
2023-11-16 18:34:44 -0300

    Style | Buttons

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className='px-8 flex flex-col  md:flex-row items-center justify-between p-2 z-40  w-full bg-blue-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20  '>
           <div className='flex items-center gap-2'>
             <Image src="nautilusLogo.svg" width={45} height={45} alt='Nautilus Logo' className='invert' />
-            <h1 className='font-semibold text-2xl text-white whitespace-nowrap'>Simulador Nautilus</h1>
+            <h1 className='font-semibold text-2xl text-white whitespace-nowrap'>Nautilus Simulator</h1>
           </div>
 
           <div className='flex gap-2 py-2'>
@@ -25,26 +25,18 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='bg-main-bg h-fit pb-32 p-12 '>
-        <div className='mt-32 flex flex-col gap-4'>
+      <div className='bg-main-bg h-fit pb-28 p-12 '>
+        <div className='mt-36 flex flex-col gap-4'>
           <h2 className='text-white text-4xl font-semibold'>&quot;Mais completo simulador de ROV do Brasil&quot; <br /> <span className='text-xl italic '>-TECROV</span></h2>
           <p className='text-white'>
             Experiencie o que há de mais moderno em simulação submarina<br />
             Com o Simulador Nautilus você se torna um real operador de ROV <br />
           </p>
           <div className='flex flex-col md:flex-row gap-2'>
-            <button className='button  bg-emerald-500 rounded-lg cursor-pointer select-none
-    active:translate-y-2  
-    active:border-b-[0px]
-    transition-all duration-150 [box-shadow:0_10px_0_0_#10b981c8]
-    border-b-[1px] border-emerald-400 whitespace-nowrap text-white w-32'>
+            <button className='bg-white bg-opacity-40 shadow px-4 py-2 rounded text-white hover:bg-opacity-70'>
               Teste grátis
             </button>
-            <button className=' button py-2 bg-blue-500 rounded-lg cursor-pointer select-none
-    active:translate-y-2  
-    active:border-b-[0px]
-    transition-all duration-150 [box-shadow:0_10px_0_0_#1b6ff8]
-    border-b-[1px] border-blue-400 whitespace-nowrap text-white w-52'>
+            <button className=' bg-blue-500 shadow bg-opacity-80 px-4 py-2 rounded text-white hover:bg-opacity-100'>
               Solicite um Orçamento
             </button>
           </div>
